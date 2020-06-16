7cb489c1262cd689a5e3507c2f6d1de1f5e6e72b
igamy <ivo.gamito@tecnico.ulisboa.pt>
2020-06-16 21:30:35 +0100

    list contexts

--- context/views.py
+++ context/views.py
@@ -5,8 +5,13 @@ from django.contrib.gis.geos import Polygon
 from .models import e_Context
 
 def show_context(request):
+    context = {
+        'contexts': e_Context.objects.all()
+    }
+
     if request.method == 'POST':
         form = ContextForm(request.POST)
+        context.update({"form":form})
         if form.is_valid():
             form.save()
             # new_context = e_Context()
@@ -16,9 +21,9 @@ def show_context(request):
             # new_context.geom = Polygon(form.cleaned_data.get('form-polygon'))
             # new_context.save()
             messages.success(request,f'Context created with success!') 
-            return render(request, 'context/context.html', {'form': form})
+            return render(request, 'context/context.html', context)
 
 
     else:
         form = ContextForm()
-    return render(request, 'context/context.html', {'form': form})
+    return render(request, 'context/context.html', context)
