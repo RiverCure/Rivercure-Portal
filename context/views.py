58c854f5a51dea6b77df700242595af431758f7f
Jorge Miguel Da Silva Marques <jms@student.dei.uc.pt>
2020-06-17 15:27:42 +0100

    cleaned context view

--- context/views.py
+++ context/views.py
@@ -6,24 +6,15 @@ from .models import e_Context
 
 def show_context(request):
     context = {
-        'contexts': e_Context.objects.all()
+        'contexts': e_Context.objects.all(),
+        'form': ContextForm()
     }
 
     if request.method == 'POST':
         form = ContextForm(request.POST)
-        context.update({"form":form})
         if form.is_valid():
             form.save()
-            # new_context = e_Context()
-            # new_context.code = form.cleaned_data.get('code')
-            # new_context.Name = form.cleaned_data.get('Name')
-            # new_context.hydroFeature = form.cleaned_data.get('hydroFeature')
-            # new_context.geom = Polygon(form.cleaned_data.get('form-polygon'))
-            # new_context.save()
             messages.success(request,f'Context created with success!') 
             return render(request, 'context/context.html', context)
 
-
-    else:
-        form = ContextForm()
     return render(request, 'context/context.html', context)
