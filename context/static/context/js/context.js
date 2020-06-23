ea43c1baceca44d87c882e5e82087f90dc63ed53
Jorge Miguel Da Silva Marques <jms@student.dei.uc.pt>
2020-06-23 18:10:59 +0100

    added markers and some rules to boundary definition

--- context/static/context/js/context.js
+++ context/static/context/js/context.js
@@ -1,5 +1,34 @@
 //Help functions
 var MyFunctions = {
+    // //Polygns to store
+    // Domain: null,
+    // Boundary: null,
+    // River: null,
+    // Refinement: null,
+    //function to configure draw control
+    drawControlConfig: (drawnPolygn) => {
+        return new L.Control.Draw({
+            //leave only polygon option in the control
+            draw: { 
+                marker: false,
+                circlemarker: false,
+                rectangle: false,
+                circle: false,
+                polygon: {
+                    shapeOptions: {
+                        fill: false
+                    }
+                },
+                polyline: {
+                    allowIntersection: false,
+                },
+            },
+            edit: {
+                featureGroup: drawnPolygn,
+                allowIntersection: false,
+            }
+        });
+    },
     //function to enable polygon drawing and disable polyline
     enablePolygon: (drawControl, polyColor, fillOption) => {
         drawControl.setDrawingOptions({
@@ -23,6 +52,31 @@ var MyFunctions = {
             },
         });
     },
+    //enable controls when users changes the select polygon
+    polygonTypeChange: (e, drawControl, domain) => {
+        console.log(domain);
+        switch(e.target.value) {
+            case "none":
+                alert("Please select a polygon type");
+                break;
+            case "Domain":
+                MyFunctions.enablePolygon(drawControl, '#C0C0C0', true);
+                break;
+            case "Boundary":
+                if(domain != null) MyFunctions.enablePolyline(drawControl, '#0000A0'); //if domain is already defined then allow the drawing of the boundary
+                else { //Domain is not defined therefore disallow user from drawing the boundary
+                    alert("Define the Domain before defining the boundaries");
+                    e.target.selectedIndex = "0";
+                }
+                break;
+            case "River":
+                MyFunctions.enablePolyline(drawControl, '#FFFF00');
+                break;
+            case "Refinement":
+                MyFunctions.enablePolygon(drawControl, '#FFA500', true);
+                break;
+        }
+    },
     //function to hadle the showing of mouse position
     showMousePosition: (map) => {
         latlngDiv = document.createElement("div");
@@ -37,6 +91,66 @@ var MyFunctions = {
         map.addEventListener('mousemove', e => {
             document.querySelector("#mouse-latlng").innerHTML = "Lat: " + e.latlng.lat.toFixed(5)+ " Lon: " + e.latlng.lng.toFixed(5);
         });
+    },
+    //function to add markers when drawing the boundary
+    drawBoundaryMarkers: (e, map, domain) => {
+        let polygonType = document.querySelector("#polygon-type").value;
+        let lat = Object.values(e.layers._layers)[Object.values(e.layers._layers).length - 1]._latlng.lat;
+        let lng = Object.values(e.layers._layers)[Object.values(e.layers._layers).length - 1]._latlng.lng;
+        if(polygonType == 'Boundary') {
+            //mark the vertex with a marker
+            L.marker([lat, lng]).addTo(map);
+            console.log(domain.getBounds());
+        }
+    },
+    //function to run on the begginning of a drawing
+    drawStart: (e) => {
+        let polygonType = document.querySelector("#polygon-type"); //select the dropdown element of the polygon type
+        polygonType.disabled = true; //disable the selection of the polygon type again
+
+        switch(polygonType.value) {
+            case "none":
+                alert("Please select a polygon type");
+                //stop drawing
+                document.querySelector('a[title="Cancel drawing"]').click();       
+                break;
+            case "River":
+            case "Boundary":
+                if(e.layerType == "polygon") {
+                    alert("Invalid shape for selected polygon");
+                    //stop drawing
+                    document.querySelector('a[title="Cancel drawing"]').click();       
+                }
+                break;
+            default:
+                if(e.layerType == "polyline") {
+                    alert("Invalid shape for selected polygon");
+                    //stop drawing
+                    document.querySelector('a[title="Cancel drawing"]').click();            
+                }
+        }
+    },
+    //function to run when draw is created
+    drawCreated: (e, createdPolygons) => {
+        drawnPolygn.addLayer(e.layer);
+        let polygonType = document.querySelector("#polygon-type").value;
+        // console.log(e.layer._bounds.contains([38.707616,-9.1365]));
+        // console.log(e.layer.getBounds());
+        //save the polygon in the appropriate variable
+        switch(polygonType) {                    
+            case "Domain":
+                createdPolygons.Domain = e.layer;
+                break;
+            case "Boundary":
+                createdPolygons.Boundary = e.layer;
+                break;
+            case "River":
+                createdPolygons.River = e.layer;
+                break;
+            case "Refinement":
+                createdPolygons.Refinement = e.layer;
+                break;
+        }
     },
 }
 
