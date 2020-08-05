60ca900e138f0896288e2f3831eca4d0fbf80cf9
igamy <ivo.gamito@tecnico.ulisboa.pt>
2020-08-05 15:04:21 +0100

    sensors - urls, pages, fields normalization

--- sensors/urls.py
+++ sensors/urls.py
@@ -1,10 +1,12 @@
 
-from .views import SensorListView
+from .views import SensorListView, SensorDetailView, SensorDeleteView, SensorUpdateView
 from django.urls import path, include
 
 urlpatterns = [
 
     path('', SensorListView, name='sensor-list'),
-    #path('hydrofeature/<int:pk>', HydroFeatureDetailView.as_view(), name='hydrofeature-detail'),
+    path('<str:pk>', SensorDetailView.as_view(), name='sensor-detail'),
+    path('<str:pk>/update/', SensorUpdateView.as_view(), name='sensor-update'),
+    path('<str:pk>/delete/', SensorDeleteView.as_view(), name='sensor-delete'),
    
 ]
