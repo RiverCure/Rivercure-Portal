a180127dc077eab91ee96454746cf50a25f1efa3
jmsmarques <jorgemsmarques9@gmail.com>
2020-09-19 22:54:27 +0100

    updated requirements.txt

--- rivercureproject/settings.py
+++ rivercureproject/settings.py
@@ -183,5 +183,9 @@ CORS_ORIGIN_ALLOW_ALL = True #This should be removed enventually
 
 # RASTER_USE_CELERY = True
 
-CELERY_BROKER_URL = 'amqp://localhost'
+CELERY_BROKER_URL = 'amqp://jorge:jorge@localhost/vjorge'
 CELERY_RESULT_BACKEND = 'django-db'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/London'
