fd7cc01b2cb5a0b65b8ab5d1b1eaa21bf6426b7f
jmsmarques <jorgemsmarques9@gmail.com>
2021-02-19 21:52:57 +0000

    Updated allowed hosts

--- rivercureproject/settings.py
+++ rivercureproject/settings.py
@@ -40,7 +40,10 @@ SECRET_KEY = '^s&4uf&m0lgol2@%+-+7714k2t)wexo3j*b98dl3w_d-$z-e@b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = [] # Must be changed to allow host in production
+ALLOWED_HOSTS = [
+    'localhost',
+    'rivercure.inesc-id.pt'
+] # Must be changed to allow host in production
 
 
 # Application definition
