922e4b863b179bd86077bb0d10bbb2da2f3df61b
ist198697 <almateser@gmail.com>
2021-10-20 10:31:10 +0100

    Trying to fix a production error II

--- sensors/views/download.py
+++ sensors/views/download.py
@@ -37,17 +37,24 @@ def increase_column_width(ws):
 # Download file
 def download_file(wb, file_name):
     relative_path = os.path.join(settings.MEDIA_ROOT, 'excels')
+    print('1')
     if not os.path.exists(relative_path):
         os.makedirs(relative_path)
-    
+    print('2')
     full_path = os.path.join(relative_path, file_name)
+    print('3')
     wb.save(full_path)
+    print('4')
     if os.path.exists(full_path):
+        print('5')
         with open(full_path, 'rb') as fh:
+            print('6')
             response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            print('7')
             response['Content-Disposition'] = 'inline; filename=' + os.path.basename(full_path)
+            print('8')
             return response
-
+    print('9')
     raise Http404
 
 def sensor_excel_download(request):
@@ -197,9 +204,6 @@ def sensor_observations_excel_download(request, sensorId):
 
         increase_column_width(ws)
 
-    print('1')
     readme(wb.active, date_str, request.user)
-    print('2')
     observations()
-    print('3')
     return download_file(wb, f'RCP_observations_{sensor.name}_create_{request.user}_{date_str}.xlsx')
