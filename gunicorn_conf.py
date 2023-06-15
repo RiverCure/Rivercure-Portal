1a3cc12bb75c021bc96a7ba799db409fa8304821
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-06-15 17:35:12 +0100

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -3,6 +3,6 @@ wsgi_app = 'rivercureproject.wsgi'
 # Workers silent for more than this many seconds are killed and restarted.
 timeout = '1800'
 workers = 4
-accesslog = '~/logs/rivercure/access.log'
+accesslog = '/home/jorge/logs/rivercure/access.log'
 acceslogformat = "%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
-errorlog = '~/logs/rivercure/error.log'
+errorlog = '/home/jorge/logs/rivercure/error.log'
