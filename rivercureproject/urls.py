e91c26225d7b621072745768797a2841afc0652b
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-06-22 19:44:35 +0100

    Update dev environment and dependencies

--- rivercureproject/urls.py
+++ rivercureproject/urls.py
@@ -31,10 +31,14 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 
     path('', include('rivercureportal.urls')),
     path('contexts/', include('context.urls')),
@@ -42,10 +46,15 @@ urlpatterns = [
     path('context/raster/', include('raster.urls')),
     path('organization/', include('organization.urls')),
 
-    url('^inbox/notifications/', include(notifications.urls, namespace='notifications')),
+    url('^inbox/notifications/',
+        include(notifications.urls, namespace='notifications')),
 
-    re_path(r'^celery-progress/', include('celery_progress.urls')),  # the endpoint is configurable
+    # the endpoint is configurable
+    re_path(r'^celery-progress/', include('celery_progress.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+if settings.DEBUG is True:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
