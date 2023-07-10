078cde9967e891c49582181e70fe6becb32808e5
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-07-10 20:00:24 +0100

    Update progress indication to new model

--- context/views/mesh.py
+++ context/views/mesh.py
@@ -9,6 +9,8 @@ from .authorization import context_organization_edit_permission_check
 from notifications.signals import notify
 
 # Renders the page that shows the progress of the mesh generation
+
+
 @login_required
 def mesh_status(request, contextCode):
     organizationCode = request.session['organizationCode']
@@ -16,59 +18,73 @@ def mesh_status(request, contextCode):
     # Authorization
     if not context_organization_edit_permission_check(request.user, context.organization):
         return HttpResponse('Unauthorized', status=401)
-    
-    return render(request, 'context/context/mesh_progress.html', { 'context': context })
+
+    return render(request, 'context/context/mesh_progress.html', {'context': context})
 
 # Return last line of output
-def get_last_line(status:str):
+
+
+def get_last_line(status: str):
     if status == "":
         return ""
-    lines:list = status.splitlines()
+    lines: list = status.splitlines()
     if lines[-1] == "" or lines[-1].isspace():
         return get_last_line("\n".join(lines[0:len(lines)-1]))
     return lines[-1]
 
-# Enhance this
-def get_status(last_line:str):
-    if ("Permission denied" in last_line) or ("Fail" in last_line) or ("Error" in last_line):
+
+def get_status(last_line: str):
+    error_substrings = [
+        "Permission denied",
+        "Fail"
+        "Error",
+        "/bin/sh: ./mesh: cannot execute binary file"
+    ]
+    finish_substrings = [
+        "all files written in",
+        "--:--:--"
+    ]
+    if last_line in error_substrings:
         return "Fail"
-    elif ("all files written in" in last_line) or ("--:--:--" in last_line):
+    elif last_line in finish_substrings:
         return "Finished successfully"
     else:
         return "Processing"
 
-# Called by the frontend as an API endpoint to get the progress of the mesh generation
+
 @login_required
 def mesh_status_progress(request, contextCode):
+    '''Function called by the frontend as an API endpoint to get the progress of the mesh generation'''
     organizationCode = request.session['organizationCode']
     context = get_object_or_404(Context, organization__code=organizationCode, code=contextCode)
-    get_object_or_404(Membership, organization=context.organization, user=request.user)
-    simulator_address = os.environ['SIMULATOR_ADDRESS']
-    url = simulator_address + 'process-status/'
+    membership = Membership.objects.filter(organization=context.organization, user=request.user)
+    if membership.count() == 0:
+        return HttpResponse(status=401)
 
-    try:
-        payload = { 'organizationCode': organizationCode, 'contextCode': contextCode }
-        response = requests.get(url, params=payload)
-        if response.status_code != 200:
-            return HttpResponse(status=400)
-        
-        body = response.content.decode("utf-8")
-        lastline = get_last_line(body)
-        status = get_status(lastline)
+    log_file = os.path.join('logs', context.tag, 'mesh_log.txt')
+    if not os.path.isfile(log_file):
+        return HttpResponse(status=404)
 
-        # Notification
-        if ("Fail" in status) or ("Finished successfully" in status):
-            notify.send(sender=context, recipient=context.requester, action_object=context.organization, verb=f"Context {context.Name} has finished its processing with status '{status}'")
+    msg = ""
+    with open(log_file, "r") as f_log:
+        msg = f_log.read()
+    lastline = get_last_line(msg)
+    status = get_status(lastline)
 
-        return JsonResponse({ 'status' : status, 'message' : lastline, 'full_log' : body })
-    except: # HiSTAV not online
-        # 503 = service unavailable
-        return HttpResponse(status=503)
+    # Notification
+    if ("Fail" in status) or ("Finished successfully" in status):
+        notify.send(sender=context, recipient=context.requester, action_object=context.organization,
+                    verb=f"Context {context.Name} has finished its processing with status '{status}'")
+
+    return JsonResponse({'status': status, 'message': lastline, 'full_log': msg})
 
 # API endpoint called by HiSTAV to notify that mesh generation has finished
 # Expected to be called like: baseUrl/mesh-status/<str:contextCode>/change?organization=<str:organizationCode>&status=<status>
+
+
 def mesh_status_change(request, contextCode):
-    organizationCode = request.GET.get('organization') # since the request comes from HiSTAV, they have to send the organization as query param
+    # since the request comes from HiSTAV, they have to send the organization as query param
+    organizationCode = request.GET.get('organization')
     context = get_object_or_404(Context, organization__code=organizationCode, code=contextCode)
     if request.GET.get('status'):
         context.hasMesh = True
@@ -80,7 +96,9 @@ def mesh_status_change(request, contextCode):
 
     return HttpResponse(status=200)
 
-# Renders the confirm regeneration of mesh page 
+# Renders the confirm regeneration of mesh page
+
+
 @login_required
 def regenerate_mesh_confirm(request, contextCode):
     organizationCode = request.session['organizationCode']
@@ -88,10 +106,12 @@ def regenerate_mesh_confirm(request, contextCode):
     # Authorization
     if not context_organization_edit_permission_check(request.user, context.organization):
         return HttpResponse('Unauthorized', status=401)
-    
-    return render(request, 'context/context/regenerate_mesh_confirm.html', { 'context': context })
+
+    return render(request, 'context/context/regenerate_mesh_confirm.html', {'context': context})
 
 # Called repeatedly by the frontend when in /context/<str:contextCode>/detail to check if the mesh has been generated
+
+
 @login_required
 def inform_mesh_status(request, contextCode):
     organizationCode = request.session['organizationCode']
