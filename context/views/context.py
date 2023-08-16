1f8c7203fbbb8dccd3559cb3d3fdcd01e15b357d
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-08-16 19:19:00 +0100

    Bug fix: arguments were in the wrong order

--- context/views/context.py
+++ context/views/context.py
@@ -342,7 +342,7 @@ def download_preprocessing_results(request, contextCode):
         messages.error(request, "File not found")
         return redirect('context-detail', contextCode=contextCode)
 
-    buf = zip_file(file_path, 'meshQuality.vtk')
+    buf = zip_file(f'{context.Name}_mesh', file_path)
     response = FileResponse(buf)
     response['Content-Disposition'] = f'attachment; filename="{context.Name}_mesh.zip"'
 
