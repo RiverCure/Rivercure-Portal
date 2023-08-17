4a38f7c92d213c9adc90673b9982c70e222a762a
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-08-17 09:47:04 +0100

    Bug fix on organization member accept

--- organization/authorization.py
+++ organization/authorization.py
@@ -1,11 +1,13 @@
 from organization.models import Membership, Organization
 from django.contrib.auth.models import User
 
+
 def belongs_to_organization(user, organization):
     try:
         return Membership.objects.filter(user=user, organization=organization, access_granted=True).exists()
     except:
         return False
+
 
 def is_org_manager(user: User, organization: Organization) -> bool:
     try:
@@ -13,17 +15,20 @@ def is_org_manager(user: User, organization: Organization) -> bool:
     except:
         return False
 
+
 def is_sensor_manager(user: User, organization: Organization) -> bool:
     try:
         return Membership.objects.filter(organization=organization, user=user, permission='org_sensorManager').exists()
     except:
         return False
 
-def is_org_manager_check(user, organizationCode):
+
+def is_org_manager_check(user: User, organization_id: int) -> bool:
     try:
-        return Membership.objects.filter(organization__code=organizationCode, user=user, permission='org_manager').exists()
+        return Membership.objects.filter(organization=organization_id, user=user, permission='org_manager').exists()
     except:
         return False
+
 
 def is_org_manager_or_sensor_manager(user: User, organization: Organization) -> bool:
     try:
--- organization/views.py
+++ organization/views.py
@@ -19,24 +19,28 @@ from django.contrib.auth.signals import user_logged_in
 from django.contrib import messages
 
 # Adds the default organization to the session, so that it is available in every view
+
+
 def user_log_in_handler(sender, user, request, **kwargs):
     organization = Profile.objects.get(user=user).defaultOrganization
     request.session['organizationCode'] = organization.code if organization else None
 
+
 user_logged_in.connect(user_log_in_handler)
+
 
 class OrganizationListView(LoginRequiredMixin, ListView):
     model = Organization
     template_name = 'organization/list.html'
     context_object_name = 'organizations'
     paginate_by = 10
-    ordering = ['-id'] # newer first
+    ordering = ['-id']  # newer first
 
     def get_context_data(self, **kwargs):
-        context = super(OrganizationListView, self).get_context_data(**kwargs) # get the default context data
+        context = super(OrganizationListView, self).get_context_data(**kwargs)  # get the default context data
 
         organizations_accepted = Organization.objects.filter(members=self.request.user, membership__access_granted=True)
-        organizations_pending  = Organization.objects.filter(members=self.request.user, membership__access_granted=False)
+        organizations_pending = Organization.objects.filter(members=self.request.user, membership__access_granted=False)
         for elem in context['organizations']:
             if elem in organizations_accepted:
                 elem.permission = Membership.objects.get(user=self.request.user, organization=elem).permission
@@ -44,6 +48,7 @@ class OrganizationListView(LoginRequiredMixin, ListView):
                 elem.pending = True
 
         return context
+
 
 class OrganizationCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     name = 'New organization'
@@ -63,11 +68,13 @@ class OrganizationCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView
         # Set manager permissions
         manager = form.cleaned_data['manager']
         # Create membership entry for manager
-        membership = Membership(user=manager, organization=organization, access_granted=True, access_grant_date=currentTime, permission='org_manager')
+        membership = Membership(user=manager, organization=organization, access_granted=True,
+                                access_grant_date=currentTime, permission='org_manager')
         membership.save()
 
         # Notify new manager
-        notify.send(sender=organization, recipient=manager, action_object=organization, verb=f"You have been assigned management of the new organization {organization.code} by {self.request.user}")
+        notify.send(sender=organization, recipient=manager, action_object=organization,
+                    verb=f"You have been assigned management of the new organization {organization.code} by {self.request.user}")
 
         return super().form_valid(form)
 
@@ -85,7 +92,7 @@ class OrganizationDetailView(LoginRequiredMixin, DetailView):
         user = self.request.user
         organization = self.get_object()
 
-        context = super(OrganizationDetailView, self).get_context_data(**kwargs) # get the default context data
+        context = super(OrganizationDetailView, self).get_context_data(**kwargs)  # get the default context data
         context['managers'] = Membership.objects.filter(organization=organization, permission='org_manager')
         context['belongsToOrganization'] = belongs_to_organization(user, organization)
         context['isManager'] = is_org_manager(user, organization)
@@ -107,12 +114,13 @@ class OrganizationUpdateView(UserPassesTestMixin, UpdateView):
     def test_func(self):
         return is_org_manager(self.request.user, self.get_object()) and self.get_object().is_active
 
+
 class OrganizationManageView(UserPassesTestMixin, ListView):
     model = User
     template_name = 'organization/manage.html'
     context_object_name = 'users'
     paginate_by = 10
-    ordering = ['-id'] # newer first
+    ordering = ['-id']  # newer first
 
     def setup(self, request, *args, **kwargs):
         notification_id = request.GET.get('notification')
@@ -132,13 +140,12 @@ class OrganizationManageView(UserPassesTestMixin, ListView):
         organization_id = self.kwargs.get('organizationId')
         organization = get_object_or_404(Organization, pk=organization_id)
 
-        context = super(OrganizationManageView, self).get_context_data(**kwargs) # get the default context data
+        context = super(OrganizationManageView, self).get_context_data(**kwargs)  # get the default context data
         context['organization'] = organization
         context['members'] = Membership.objects.filter(organization=organization)
 
         return context
 
-    
     def handle_no_permission(self):
         if self.request.user.is_authenticated:
             return redirect('organization-list')
@@ -157,36 +164,42 @@ class MemberRoleUpdateView(UserPassesTestMixin, UpdateView):
     def get_success_url(self):
         user = self.get_object().user
         organization = self.get_object().organization
-        
+
         newRole = Membership.objects.get(user=user, organization=organization).get_permission_display()
-        
+
         # Notify other managers
-        managers = User.objects.filter(membership__in=Membership.objects.filter(organization=organization, permission='org_manager')).exclude(pk=self.request.user.id)
-        notify.send(sender=organization, recipient=managers, action_object=organization, verb=f"User {user} role on organization {organization.code} has been changed to {newRole} by {self.request.user}")
+        managers = User.objects.filter(membership__in=Membership.objects.filter(
+            organization=organization, permission='org_manager')).exclude(pk=self.request.user.id)
+        notify.send(sender=organization, recipient=managers, action_object=organization,
+                    verb=f"User {user} role on organization {organization.code} has been changed to {newRole} by {self.request.user}")
         # Notify user
-        notify.send(sender=organization, recipient=user, action_object=organization, verb=f"Your role on organization {organization.code} has been changed to {newRole} by {self.request.user}")
+        notify.send(sender=organization, recipient=user, action_object=organization,
+                    verb=f"Your role on organization {organization.code} has been changed to {newRole} by {self.request.user}")
 
         return reverse('organization-manage', args=(self.get_object().organization.id,))
 
     def test_func(self):
         organization = self.get_object().organization
-        cond1 = self.request.user in User.objects.filter(membership__in=Membership.objects.filter(organization=organization, permission='org_manager'))
-        cond2 = Membership.objects.filter(organization=organization, permission='org_manager').count() == 1 and self.get_object().permission == 'org_manager' and is_platform_admin(self.request.user)
+        cond1 = self.request.user in User.objects.filter(
+            membership__in=Membership.objects.filter(organization=organization, permission='org_manager'))
+        cond2 = Membership.objects.filter(organization=organization, permission='org_manager').count(
+        ) == 1 and self.get_object().permission == 'org_manager' and is_platform_admin(self.request.user)
         return (cond1 or cond2) and organization.is_active
+
 
 class OrganizationManageManagersView(UserPassesTestMixin, ListView):
     model = User
     template_name = 'organization/manage_managers.html'
     context_object_name = 'users'
     paginate_by = 10
-    ordering = ['-id'] # newer first
+    ordering = ['-id']  # newer first
 
     def test_func(self):
         return is_platform_admin(self.request.user)
 
     def get_context_data(self, **kwargs):
-        context = super(OrganizationManageManagersView, self).get_context_data(**kwargs) # get the default context data
-        
+        context = super(OrganizationManageManagersView, self).get_context_data(**kwargs)  # get the default context data
+
         organization_id = self.kwargs.get('organizationId')
         organization = get_object_or_404(Organization, pk=organization_id)
         context['organization'] = organization
@@ -194,20 +207,24 @@ class OrganizationManageManagersView(UserPassesTestMixin, ListView):
 
         return context
 
+
 @login_required
 def organizationAccessRequest(request, organizationId):
 
     organization = get_object_or_404(Organization, pk=organizationId)
     user = request.user
-    
+
     if (not Membership.objects.filter(user=user, organization=organization).exists()) and organization.is_active == True:
         accessRequest = Membership(organization=organization, user=user)
         accessRequest.save()
 
-        managers = User.objects.filter(membership__in=Membership.objects.filter(organization_id=organizationId, permission='org_manager'))
-        notify.send(sender=user, recipient=managers, action_object=organization, verb=f"{user.username} requested to enter the organization {organization.code}")
+        managers = User.objects.filter(membership__in=Membership.objects.filter(
+            organization_id=organizationId, permission='org_manager'))
+        notify.send(sender=user, recipient=managers, action_object=organization,
+                    verb=f"{user.username} requested to enter the organization {organization.code}")
 
     return redirect('organization-list')
+
 
 @login_required
 def organizationAccessRequestCancel(request, organizationId):
@@ -219,11 +236,12 @@ def organizationAccessRequestCancel(request, organizationId):
     if membership_user and organization.is_active == True:
         membership_user.delete()
 
-    notifications = Notification.objects.filter(verb=f"{user.username} requested to enter the organization {organization.code}")
+    notifications = Notification.objects.filter(
+        verb=f"{user.username} requested to enter the organization {organization.code}")
     if notifications is not None:
         notifications.first().mark_as_read()
 
-    return redirect('organization-list') 
+    return redirect('organization-list')
 
 
 @login_required
@@ -232,19 +250,23 @@ def organizationAccessRequestDeny(request, organizationId, userId):
         return HttpResponseRedirect(reverse('rivercure-home'))
 
     organization = get_object_or_404(Organization, pk=organizationId)
-    user = get_object_or_404(User, pk=userId) # requester
+    user = get_object_or_404(User, pk=userId)  # requester
     membership_user = get_object_or_404(Membership, user=user, organization=organization)
 
     if membership_user and organization.is_active == True:
         membership_user.delete()
-    
+
         # Notify the user
-        notify.send(sender=organization, recipient=user, action_object=organization, verb=f"You have been denied access to organization {organization.code}")
+        notify.send(sender=organization, recipient=user, action_object=organization,
+                    verb=f"You have been denied access to organization {organization.code}")
         # Notify all managers of the organization
-        managers = User.objects.filter(membership__in=Membership.objects.filter(organization=organization, permission='org_manager')).exclude(pk=request.user.id)
-        notify.send(sender=organization, recipient=managers, action_object=organization, verb=f"User {user} has been denied access to organization {organization.code}")
+        managers = User.objects.filter(membership__in=Membership.objects.filter(
+            organization=organization, permission='org_manager')).exclude(pk=request.user.id)
+        notify.send(sender=organization, recipient=managers, action_object=organization,
+                    verb=f"User {user} has been denied access to organization {organization.code}")
 
     return redirect('organization-manage', organizationId)
+
 
 @login_required
 def organizationAccessRemove(request, organizationId, userId):
@@ -259,16 +281,20 @@ def organizationAccessRemove(request, organizationId, userId):
     # Don't allow the last manager to be removed
     if Membership.objects.filter(organization=organization, permission='org_manager').count() == 1 and membership_user.permission == 'org_manager':
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-        
+
     if membership_user and organization.is_active == True:
         membership_user.delete()
 
-        notify.send(sender=organization, recipient=user, action_object=organization, verb=f"Your access to organization {organization.code} has been removed")
+        notify.send(sender=organization, recipient=user, action_object=organization,
+                    verb=f"Your access to organization {organization.code} has been removed")
         # Notify all managers of the organization
-        managers = User.objects.filter(membership__in=Membership.objects.filter(organization=organization, permission='org_manager')).exclude(pk=request.user.id)
-        notify.send(sender=organization, recipient=managers, action_object=organization, verb=f"User {user} access to organization {organization.code} has been removed")
+        managers = User.objects.filter(membership__in=Membership.objects.filter(
+            organization=organization, permission='org_manager')).exclude(pk=request.user.id)
+        notify.send(sender=organization, recipient=managers, action_object=organization,
+                    verb=f"User {user} access to organization {organization.code} has been removed")
 
     return redirect('organization-manage', organizationId)
+
 
 @login_required
 def organizationAccessAllow(request, organizationId, userId):
@@ -277,7 +303,7 @@ def organizationAccessAllow(request, organizationId, userId):
 
     organization = get_object_or_404(Organization, pk=organizationId)
     user = get_object_or_404(User, pk=userId)
-    
+
     membership_user = Membership.objects.filter(user=user, organization=organization).first()
     if membership_user and organization.is_active == True:
         print(membership_user.access_granted)
@@ -288,12 +314,16 @@ def organizationAccessAllow(request, organizationId, userId):
         membership_user.save()
 
         # Notify the user
-        notify.send(sender=organization, recipient=user, action_object=organization, verb=f"You have been granted access to organization {organization.code}")
+        notify.send(sender=organization, recipient=user, action_object=organization,
+                    verb=f"You have been granted access to organization {organization.code}")
         # Notify all managers of the organization
-        managers = User.objects.filter(membership__in=Membership.objects.filter(organization=organization, permission='org_manager')).exclude(pk=request.user.id)
-        notify.send(sender=organization, recipient=managers, action_object=organization, verb=f"User {user} has been granted access to organization {organization.code}")
+        managers = User.objects.filter(membership__in=Membership.objects.filter(
+            organization=organization, permission='org_manager')).exclude(pk=request.user.id)
+        notify.send(sender=organization, recipient=managers, action_object=organization,
+                    verb=f"User {user} has been granted access to organization {organization.code}")
 
     return redirect('organization-manage', organizationId)
+
 
 @login_required
 @user_passes_test(is_platform_admin)
@@ -303,10 +333,13 @@ def organizationReactivate(request, organizationId):
     organization.save()
 
     # Notify all members of the organization
-    members = User.objects.filter(membership__in=Membership.objects.filter(organization=organization, access_granted=True)).exclude(pk=request.user.id)
-    notify.send(sender=organization, recipient=members, action_object=organization, verb=f"Your organization {organization.code} has been reactivated by {request.user}")
+    members = User.objects.filter(membership__in=Membership.objects.filter(
+        organization=organization, access_granted=True)).exclude(pk=request.user.id)
+    notify.send(sender=organization, recipient=members, action_object=organization,
+                verb=f"Your organization {organization.code} has been reactivated by {request.user}")
 
     return redirect('organization-list')
+
 
 @login_required
 @user_passes_test(is_platform_admin)
@@ -316,10 +349,13 @@ def organizationSuspend(request, organizationId):
     organization.save()
 
     # Notify all members of the organization
-    members = User.objects.filter(membership__in=Membership.objects.filter(organization=organization, access_granted=True)).exclude(pk=request.user.id)
-    notify.send(sender=organization, recipient=members, action_object=organization, verb=f"Your organization {organization.code} has been suspended by {request.user}")
+    members = User.objects.filter(membership__in=Membership.objects.filter(
+        organization=organization, access_granted=True)).exclude(pk=request.user.id)
+    notify.send(sender=organization, recipient=members, action_object=organization,
+                verb=f"Your organization {organization.code} has been suspended by {request.user}")
 
     return redirect('organization-list')
+
 
 @login_required
 def organizationSetCurrent(request, organizationId):
