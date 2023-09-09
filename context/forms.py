b233264e080ae8352e30ee7aa5c6457c6c1468b5
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-09-09 10:48:07 +0100

    Constraint the name and code of a context

--- context/forms.py
+++ context/forms.py
@@ -1,5 +1,6 @@
+import re
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 from django.contrib.gis.forms import fields
 from .models import e_Context, e_ContextEvent
 from .models import e_HydroFeature
@@ -7,24 +8,51 @@ from leaflet.forms.widgets import LeafletWidget
 from organization.models import Organization, Membership
 from django.db.models import Q
 
+
 class ContextDetailsForm(forms.ModelForm):
 
     hydroFeature = forms.ModelChoiceField(queryset=e_HydroFeature.objects.all(), required=False)
+
+    def clean_Name(self):
+        pattern = re.compile('^[\\w]+[-\\w]*$')
+        data: str = self.cleaned_data['Name']
+        if not bool(pattern.match(data)):
+            raise ValidationError("Name must only have letters, numbers. These can be intercalated with slashes (-)")
+
+        return data
+
     class Meta:
         model = e_Context
-        fields = ['Name','hydroFeature','isPublic']
+        fields = ['Name', 'hydroFeature', 'isPublic']
+
 
 class ContextInitialForm(forms.ModelForm):
     class Meta:
         model = e_Context
-        fields = ['code','Name', 'hydroFeature', 'organization', 'isPublic',]
+        fields = ['code', 'Name', 'hydroFeature', 'organization', 'isPublic',]
+
+    def clean_code(self):
+        pattern = re.compile('^[\\w]+[-\\w]*$')
+        data: str = self.cleaned_data['code']
+        if not bool(pattern.match(data)):
+            raise ValidationError("Code must only have letters, numbers. These can be intercalated with slashes (-)")
+
+    def clean_Name(self):
+        pattern = re.compile('^[\\w]+[-\\w]*$')
+        data: str = self.cleaned_data['Name']
+        if not bool(pattern.match(data)):
+            raise ValidationError("Name must only have letters, numbers. These can be intercalated with slashes (-)")
+
+        return data
 
     def __init__(self, *args, **kwargs):
         user_id = kwargs.pop('user_id')
         super(ContextInitialForm, self).__init__(*args, **kwargs)
         # We only want to allow to choose options where the user is org_manager or org_contextManager of the organization
-        memberships = Membership.objects.filter(user_id=user_id, access_granted=True, organization__is_active=True).filter(Q(permission='org_manager') | Q(permission='org_contextManager'))
+        memberships = Membership.objects.filter(user_id=user_id, access_granted=True, organization__is_active=True).filter(
+            Q(permission='org_manager') | Q(permission='org_contextManager'))
         self.fields['organization'].queryset = Organization.objects.filter(membership__in=memberships)
+
 
 class ContextForm(forms.Form):
     code = forms.CharField(widget=forms.HiddenInput())
@@ -33,6 +61,7 @@ class ContextForm(forms.Form):
     refinement = forms.CharField(widget=forms.HiddenInput())
     boundaries = forms.CharField(widget=forms.HiddenInput())
     boundary_points = forms.CharField(widget=forms.HiddenInput())
+
 
 class UploadContextForm(forms.Form):
     code = forms.CharField(widget=forms.HiddenInput())
@@ -43,11 +72,14 @@ class UploadContextForm(forms.Form):
     dtm_file = forms.FileField(required=False)
     friction_coefficient_file = forms.FileField(required=False)
 
+
 class EventForm(forms.ModelForm):
     context_code = forms.CharField(widget=forms.HiddenInput())
+
     class Meta:
         model = e_ContextEvent
-        fields = ['Name', 'type', 'subtype', 'context_code', 'startDate', 'startTime', 'endDate', 'endTime', 'description', 'returnPeriod', 'warmUp', 'WritingPeriodicity', 'WritingPeriodicityUnit', 'UpdateMaximumValue', 'UpdateMaximumValueUnit']
+        fields = ['Name', 'type', 'subtype', 'context_code', 'startDate', 'startTime', 'endDate', 'endTime', 'description',
+                  'returnPeriod', 'warmUp', 'WritingPeriodicity', 'WritingPeriodicityUnit', 'UpdateMaximumValue', 'UpdateMaximumValueUnit']
         widgets = {
             'startDate': forms.TextInput(attrs={'placeholder': 'yyyy-mm-dd'}),
             'endDate': forms.TextInput(attrs={'placeholder': 'yyyy-mm-dd'}),
@@ -55,7 +87,7 @@ class EventForm(forms.ModelForm):
             'endTime': forms.TextInput(attrs={'placeholder': 'hh:mm:ss'}),
             'description': forms.Textarea(attrs={'placeholder': 'Enter description here'}),
         }
-    
+
     def clean(self):
         super(EventForm, self).clean()
 
