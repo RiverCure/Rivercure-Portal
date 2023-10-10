21f1700fb751ea350a334112c0d217bd8f1767e3
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-10-10 13:46:24 +0100

    Fix circular import

--- context/signals.py
+++ context/signals.py
@@ -1,14 +1,15 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from context.models import e_ContextEvent
+from rivercureproject.celery import app
 
 
 @receiver(pre_save, sender=e_ContextEvent)
-def generate_tiffs(sender: e_ContextEvent, instance, **kwargs):
+def generate_tiffs_signal(sender: e_ContextEvent, instance, **kwargs):
     try:
         obj = sender.objects.get(pk=instance.pk)
     except sender.DoesNotExist:
         pass  # Object is new, so field hasn't technically changed, but you may want to do something else here.
     else:
         if obj.hasSimulation != instance.hasSimulation:  # Field has changed
-            generate_tiffs.delay(obj.pk)
+            app.send_task('tasks.generate_tiffs', args=(obj.pk, ))
