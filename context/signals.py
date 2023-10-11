ccc26f2bf915bc17ffe323f8694b6b8245d34475
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-10-11 18:40:52 +0100

    Only generate tiffs if hasSimulation is true

--- context/signals.py
+++ context/signals.py
@@ -11,5 +11,5 @@ def generate_tiffs_signal(sender: e_ContextEvent, instance, **kwargs):
     except sender.DoesNotExist:
         pass  # Object is new, so field hasn't technically changed, but you may want to do something else here.
     else:
-        if obj.hasSimulation != instance.hasSimulation:  # Field has changed
+        if obj.hasSimulation != instance.hasSimulation and instance.hasSimulation == True:  # Field has changed
             app.send_task('context.tasks.generate_tiffs', args=(obj.pk, ))
