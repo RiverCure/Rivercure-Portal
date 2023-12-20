a24523541777ec959168ccb631ef9d70df76b575
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-12-20 11:34:02 +0000

    Increase max zoom

--- rivercureproject/settings.py
+++ rivercureproject/settings.py
@@ -149,7 +149,7 @@ LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (38.707616, -9.1365),  # Lisbon coordinates
     'DEFAULT_ZOOM': 6,
     'MIN_ZOOM': 3,
-    'MAX_ZOOM': 20,
+    'MAX_ZOOM': 30,
     'RESET_VIEW': False,
     'TILES': [('Satellite', 'https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/512/{z}/{x}/{y}@2x?access_token={accessToken}', {
         'id': 'satellite-streets-v11',
