e0269dadae01abfce4da9db5fd70564ef35c90fe
mafyay <mafaldacravo@gmail.com>
2024-03-20 17:19:49 +0000

    Added has_org check to show Management dropdown in nav bar

--- rivercureportal/templatetags/auth_extras.py
+++ rivercureportal/templatetags/auth_extras.py
@@ -1,9 +1,16 @@
 from django import template
 from django.contrib.auth.models import Group 
 
+from organization.models import Membership
+
 register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name): 
     group = Group.objects.get(name=group_name) 
     return True if group in user.groups.all() else False
+
+# Check if user belongs to any organization
+@register.filter(name="has_org")
+def has_org(user):
+    return Membership.objects.filter(user=user).exists()
